818f8622e57de754f2346ce359afafb2b91fe503
joechiboo <joe.chi@lovee.be>
2016-10-19 23:25:46 +0800

    add MarriedCtrl

---
+++ Resume/Controllers/MarriedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Resume.Controllers
+{
+    public class MarriedController : Controller
+    {
+        // GET: Married
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
