9e018a9288aeace78796d96726237e216c6223ad
joechiboo <joe.chi@lovee.be>
2017-01-24 21:42:56 +0800

    add Filter function

---
+++ Resume/Controllers/api/TablesController.cs
@@ -0,0 +1,26 @@
+﻿using Resume.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Resume.Controllers.api
+{
+    public class TablesController : ApiController
+    {
+        private MarriedContext db = new MarriedContext();
+
+        public IQueryable<string> GetTalkTables() {
+            var result = from c in db.ChatLogs
+                         join m in db.Members on c.MemberId equals m.id
+                         join i in db.Infomations on c.MemberId equals i.memberid
+                         where m.Valid == 1 
+                            && i.tableid != null
+                         select i.tableid;
+
+            return result.Distinct();
+        }
+    }
+}
--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,8 +1,15 @@
 
-
 --select * from Members with(nolock)
 --select * from ChatLogs with(nolock)
---select * from Information with(nolock)
+--select * from Information with(nolock) order by memberid
+select * from [Tables] with(nolock)
+
+update [Tables] set Name = N'303®à' where id = 33
+
+update Information set tableid = 26 where memberid in (17, 20)
+update Information set tableid = 27 where memberid in (22)
+update Information set tableid = 30 where memberid in (23)
+update Members set Valid = 1 where id in (33)
 
 --- message log
 select m.Name, c.[message] from ChatLogs as c with(nolock)
@@ -24,3 +31,19 @@ order by m.id
 select Sum(number) as N'»««ÈÁ`¼Æ', Sum(vegetable) as N'¯À­¹' from Information as i with(nolock) 
 inner join Members as m with(nolock) on i.memberid = m.id
 where m.Valid = 1
+
+--- number per table
+select i.tableid, Sum(i.number) from Members as m
+	inner join Information as i on m.id = i.memberid
+where m.Valid = 1
+group by i.tableid
+
+--- talk members
+select 
+	--c.memberid, 
+	distinct i.tableid 
+from ChatLogs as c with(nolock)
+	inner join Members as m with(nolock) on c.memberid = m.id
+	inner join Information as i with(nolock) on m.id = i.memberid
+where m.Valid = 1
+	and tableid is not null
