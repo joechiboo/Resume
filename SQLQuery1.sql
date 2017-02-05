559537cd9841f0bb2b20b42631f8b42667b08ad6
joechiboo <joe.chi@lovee.be>
2017-02-05 22:08:21 +0800

    Seat formal

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,14 +2,11 @@
 --select * from Members with(nolock)
 --select * from ChatLogs with(nolock)
 --select * from Information with(nolock) order by memberid
-select * from [Tables] with(nolock)
+--select * from [Tables] with(nolock)
 
-update [Tables] set Name = N'303桌' where id = 33
-
-update Information set tableid = 26 where memberid in (17, 20)
-update Information set tableid = 27 where memberid in (22)
-update Information set tableid = 30 where memberid in (23)
-update Members set Valid = 1 where id in (33)
+--update [Tables] set Name = N'恩師' where id = 24
+--update Information set tableid = 24 where memberid in (52)
+--update Members set Valid = 1 where id = 52
 
 --- message log
 select m.Name, c.[message] from ChatLogs as c with(nolock)
@@ -23,13 +20,13 @@ select
 	number,
 	tableId
 from Information as i with(nolock)
-inner join Members as m with(nolock) on i.memberid = m.id
+	inner join Members as m with(nolock) on i.memberid = m.id
 where m.Valid = 1 or m.Valid is null
 order by m.id
 
 --- total comming
 select Sum(number) as N'賓客總數', Sum(vegetable) as N'素食' from Information as i with(nolock) 
-inner join Members as m with(nolock) on i.memberid = m.id
+	inner join Members as m with(nolock) on i.memberid = m.id
 where m.Valid = 1
 
 --- number per table
@@ -37,6 +34,30 @@ select i.tableid, Sum(i.number) from Members as m
 	inner join Information as i on m.id = i.memberid
 where m.Valid = 1
 group by i.tableid
+
+--- list roster per table
+select 
+	i.tableid,
+	i.number,
+	m.name 
+from Members as m with(nolock)
+	inner join Information as i with(nolock) on m.id = i.memberid
+where m.Valid = 1
+	and i.number > 0
+	and i.tableid is not null
+order by i.tableid
+
+--- have no setting table
+select 
+	i.tableid,
+	m.id,
+	m.name,
+	i.number
+from Members as m
+	inner join Information as i on m.id = i.memberid
+where m.Valid = 1
+	and i.tableid is null
+	and i.number > 0
 
 --- talk members
 select 
