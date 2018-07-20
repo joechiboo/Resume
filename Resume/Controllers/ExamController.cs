1f81a86ca9abe64bcb7a44863dccee52134f5ea5
joechiboo <stst1239joe@hotmail.com>
2018-07-20 09:13:47 +0800

    Create ExamController.cs

---
+++ Resume/Controllers/ExamController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Resume.Controllers
+{
+    public class ExamController : Controller
+    {
+        //
+        // GET: /Exam/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
